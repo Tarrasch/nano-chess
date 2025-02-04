9dfe6e2f14d2f0e731508b91de28bc4241fbf095
Arash Rouhani <arash.rouhani@gmail.com>
2025-02-04 23:27:43 +0700

    Add recapture quiescence_search

--- src/search.py
+++ src/search.py
@@ -1,7 +1,7 @@
 import dataclasses
 import time
 from position import Position, Move
-from typing import Iterable
+from typing import Iterable, Optional
 
 WINNING_SCORE = 150000
 
@@ -49,21 +49,69 @@ def search_with_depth(position: Position, depth: int, color: str) -> str:
     best_move = search_result.move
     if position.is_flipped_perspective:
         best_move = best_move.rotate()  # type: ignore
-    # print("position_score = ", position_score)
 
     return best_move.to_uci()  # type: ignore
 
 
-def negamax(position: Position, depth: int, alpha: int, beta: int) -> SearchResult:
-    if position.king_is_captured or depth <= 0:
+def quiescence_search(
+    position: Position, alpha: int, beta: int, last_captured_square: int
+) -> int:
+    # From https://www.chessprogramming.org/Quiescence_Search :)
+    stand_pat = position.evaluation
+    best_score = stand_pat
+    if stand_pat >= beta:
+        return stand_pat
+    if alpha < stand_pat:
+        alpha = stand_pat
+    for move in position.gen_moves():
+        if move.j != last_captured_square:
+            continue
+        new_position = position.move(move)
+        child_score = -quiescence_search(
+            new_position, -beta, -alpha, 119-last_captured_square
+        )
+
+        if child_score > best_score:
+            best_score = child_score
+        alpha = max(alpha, child_score)
+        if alpha >= beta:
+            break
+    return best_score
+
+
+def negamax(
+    position: Position,
+    depth: int,
+    alpha: int,
+    beta: int,
+    last_captured_square: Optional[int] = None,
+) -> SearchResult:
+    if position.king_is_captured:
         return SearchResult.from_terminal_node(score=position.evaluation)
+    if depth <= 0:
+        if last_captured_square:
+            return SearchResult.from_terminal_node(
+                score=quiescence_search(
+                    position, alpha, beta, last_captured_square=last_captured_square
+                )
+            )
+        else:
+            return SearchResult.from_terminal_node(score=position.evaluation)
 
     final_search_result = SearchResult(
         depth=depth, score=-1000000, nodes_visited=0, move=None
     )
     for move in position.gen_moves():
+        is_capture = position.board[move.j].islower()
         new_position = position.move(move)
-        child_search_result = negamax(new_position, depth - 1, -beta, -alpha)
+        last_captured_square = 119-move.j if is_capture else None
+        child_search_result = negamax(
+            new_position,
+            depth - 1,
+            -beta,
+            -alpha,
+            last_captured_square=last_captured_square,
+        )
         final_search_result.nodes_visited += child_search_result.nodes_visited
 
         score = -child_search_result.score
